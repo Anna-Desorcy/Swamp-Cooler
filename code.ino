5e1a5cf73a941cd1c700ed7e05d62ecb3196005d
Anna <105665732+Anna-Desorcy@users.noreply.github.com>
2023-12-08 17:49:58 -0800

    Add files via upload
    Added logic for LEDs

--- code.ino
+++ code.ino
@@ -16,11 +16,19 @@ float humidity = 0;
 int waterLevel = 0;
 int waterPin = A5;
 
+int red_led = 44;
+int yellow_led = 42;
+int green_led = 40;
+
 void setup() {
   dht.begin();
 
   lcd.begin(16,2);
   lcd.print("Booting Up...");
+
+  DDRL |= (1 << DDL5); // red_led
+  DDRL |= (1 << DDL4); // yellow_led
+  DDRL |= (1 << DDL2); // green_led
 
 }
 
@@ -46,12 +54,27 @@ void loop() {
     lcd.print("Humidity: ");
     lcd.print(humidity);
     lcd.print(" %");
+
+    PORTL |= (1 << PORTL5); // red_led HIGH
+    PORTL &= ~(1 << PORTL4); // yellow_led LOW
+    PORTL &= ~(1 << PORTL2); // green_led LOW
   }
-  if(waterLevel > 100 & waterLevel <= 300){
+  if(waterLevel > 100 & waterLevel <= 250){
     lcd.clear();
     lcd.setCursor(0, 0);
+
+    PORTL &= ~(1 << PORTL5); // red_led LOW
+    PORTL |= (1 << PORTL4);  // yellow_led HIGH
+    PORTL &= ~(1 << PORTL2); // green_led LOW
+
     lcd.print("Water Level Low");
     }
+  else if(waterLevel > 250){
+
+    PORTL &= ~(1 << PORTL5); // red_led LOW
+    PORTL &= ~(1 << PORTL4); // yellow_led LOW
+    PORTL |= (1 << PORTL2);  // green_led HIGH
+  }
 }
 
 
